17190643a2c69dc36d7f0f7bb9eeb198d3280b86
giselleLago <gisellelago25994@gmail.com>
2020-07-18 14:00:53 +0200

    Add project files.

---
+++ CamelCase/Converter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CamelCase
+{
+    public class Converter
+    {
+        public string CamelCase(string str)
+        {
+            var res = new List<char>();
+            var isLetter = true;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i]) && isLetter == false)
+                {
+                    res.Add(char.ToUpper(str[i]));
+                    isLetter = true;
+                    
+                }
+                else if (char.IsLetter(str[i]) && isLetter != false)
+                {
+                    res.Add(char.ToLower(str[i]));
+                }
+                else
+                {
+                    isLetter = false;
+                }
+                
+            } 
+            return string.Join("", res);
+
+        }
+    }
+}
---
+++ CamelCase/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Converter();
+            Console.WriteLine(result.CamelCase("a b c d-e-f%g"));
+            Console.ReadLine();
+        }
+    }
+}
